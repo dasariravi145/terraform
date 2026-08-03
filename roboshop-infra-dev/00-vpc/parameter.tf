f8b275ec2afc1c3ca40db3e4acc3790086ed5033
unknown <dasariravid145@gmail.com>
2026-08-03 21:53:50 +0530

    terraform

--- roboshop-infra-dev/00-vpc/parameter.tf
+++ roboshop-infra-dev/00-vpc/parameter.tf
@@ -1,4 +1,4 @@
-resource "aws_ssm_parameter" "mongodb_id" {
+resource "aws_ssm_parameter" "mongodb" {
   name  = "/${var.project}/${var.environment}/vpc_id"
   type  = "String"
   value = module.vpc.vpc_id
